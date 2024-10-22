3bd18e498c515d277782171af4dde917a8af0957
dungnguyen <dungnguyen>
2024-10-22 22:59:31 +0700

    change KOL and Graph class

--- src/main/java/model/KOL.java
+++ src/main/java/model/KOL.java
@@ -1,9 +1,21 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import java.util.List;
 
 public class KOL extends User{
     private Double pagerankScore;
+    Map<KOL, Map<String, Double>> Interaction = new HashMap<>();
+
+    public void setInteraction(Map<KOL, Map<String, Double>> Interaction){
+        this.Interaction = Interaction;
+    }
+
+    public Map<KOL, Map<String, Double>> getInteraction(){
+        return Interaction;
+    }
     public KOL() {
 
     }
