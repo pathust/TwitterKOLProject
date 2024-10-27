f96ca725ac6e4c087a2610bb25e0bbbfd07d655e
Anh Tai Phan <taiphananh28@gmail.com>
2024-10-28 01:45:07 +0700

    refactor

--- src/main/java/scraper/TwitterScraper.java
+++ src/main/java/scraper/TwitterScraper.java
@@ -36,13 +36,8 @@ public class TwitterScraper {
     }
 
     private String getUserProfileLink(WebElement userCell) {
-        try {
-            return userCell.findElement(
-                    By.xpath(".//a[@role='link']")).getAttribute("href");
-        } catch (NoSuchElementException e) {
-            System.out.println("Unable to find profile link.");
-            return "";
-        }
+        return userCell.findElement(
+                By.xpath(".//a[@role='link']")).getAttribute("href");
     }
 
     private void addUserToJson(String profileLink, boolean verified) {
@@ -52,138 +47,115 @@ public class TwitterScraper {
         userArray.add(userNode);
     }
 
-    private void saveJsonToFile(String filename) {
-        try {
-            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(filename), userArray);
-            System.out.println("Data saved to " + filename);
-        } catch (IOException e) {
-            System.out.println("Error saving JSON file: " + e.getMessage());
-        }
+    private void saveJsonToFile(String filename) throws IOException {
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File(filename), userArray);
+        System.out.println("Data saved to " + filename);
     }
 
-    private void extractInitialKOLsToJson(String filename) {
-        try {
-            System.out.println("Start collecting user data...");
+    private void extractUserToJson(String filename) throws InterruptedException, IOException {
+        for (int scrollCount = 0; scrollCount < MAX_SCROLLS; scrollCount++) {
+            Thread.sleep(3000);
+            List<WebElement> userCells = findNextUserCells();
 
-            navigator.clickButton("People");
-            for (int scrollCount = 0; scrollCount < MAX_SCROLLS; scrollCount++) {
-                Thread.sleep(3000);
-                List<WebElement> userCells = findNextUserCells();
-
-                if (userCells.isEmpty()) {
-                    System.out.println("No user cells found.");
-                    break;
-                }
-
-                for (int i = 0; i < userCells.size(); i++) {
-                    WebElement userCell = wait.until(presenceOfElementLocated(
-                            By.xpath("(//button[@data-testid='UserCell'])[" + (i + 1) + "]")));
-                    String profileLink = getUserProfileLink(userCell);
-
-                    if (!profileLink.isEmpty()) {
-                        addUserToJson(profileLink, true);
-                    }
-                }
-                navigator.scrollDown();
+            if (userCells.isEmpty()) {
+                System.out.println("No user cells found.");
+                break;
             }
 
-            saveJsonToFile(filename);
-        } catch (Exception e) {
-            System.out.println("Error processing users: " + e.getMessage());
+            for (int i = 0; i < userCells.size(); i++) {
+                WebElement userCell = wait.until(presenceOfElementLocated(
+                        By.xpath("(//button[@data-testid='UserCell'])[" + (i + 1) + "]")));
+                String profileLink = getUserProfileLink(userCell);
+
+                if (!profileLink.isEmpty()) {
+                    addUserToJson(profileLink, true);
+                }
+            }
+            navigator.scrollDown();
         }
+
+        saveJsonToFile(filename);
     }
 
-    public List<String> getUserLinksFromJson(String filePath) {
+    private void extractInitialKOLsToJson(String filename) throws InterruptedException, IOException {
+        System.out.println("Start collecting user data...");
+
+        navigator.clickButton("People");
+
+        extractUserToJson(filename);
+    }
+
+    public List<String> getUserLinksFromJson(String filePath) throws IOException {
         List<String> userLinks = new ArrayList<>();
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode rootNode = mapper.readTree(new File(filePath));
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(new File(filePath));
 
-            for (JsonNode userNode : rootNode) {
-                String url = userNode.path("profileLink").asText();
-                if (!url.isEmpty()) {
-                    userLinks.add(url);
-                }
+        for (JsonNode userNode : rootNode) {
+            String url = userNode.path("profileLink").asText();
+            if (!url.isEmpty()) {
+                userLinks.add(url);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         return userLinks;
     }
 
-    private void extractPotentialKOLs() {
-        try {
-            Thread.sleep(3000);
-            List<WebElement> retweets = wait.until(presenceOfAllElementsLocatedBy(By.xpath("//span[@data-testid='socialContext']")));
-            System.out.println("Number of retweets found: " + retweets.size());
-            if (retweets.isEmpty()) {
-                return;
-            }
+    private void extractPotentialKOLs() throws InterruptedException {
+        Thread.sleep(3000);
+        List<WebElement> retweets = wait.until(presenceOfAllElementsLocatedBy(By.xpath("//span[@data-testid='socialContext']")));
+        System.out.println("Number of retweets found: " + retweets.size());
+        if (retweets.isEmpty()) {
+            return;
+        }
 
-            List<WebElement> potentialKOLs = driver.findElements(By.xpath("//span[@data-testid='socialContext']/following::span[2]"));
-            System.out.println("Number of potential KOLs found: " + potentialKOLs.size());
+        List<WebElement> potentialKOLs = driver.findElements(By.xpath("//span[@data-testid='socialContext']/following::span[2]"));
+        System.out.println("Number of potential KOLs found: " + potentialKOLs.size());
 
-            for (WebElement potentialKOL : potentialKOLs) {
-                String kolName = potentialKOL.getText();
-                WebElement dateElement = potentialKOL.findElement(By.xpath("following::time[1]"));
-                String postedDate = dateElement.getAttribute("datetime");
+        for (WebElement potentialKOL : potentialKOLs) {
+            String kolName = potentialKOL.getText();
+            WebElement dateElement = potentialKOL.findElement(By.xpath("following::time[1]"));
+            String postedDate = dateElement.getAttribute("datetime");
 
-                System.out.println("Potential KOL: " + kolName);
-                System.out.println("Tweet's posted date: " + postedDate);
-            }
-        } catch (NoSuchElementException e) {
-            System.out.println("No reposts found.");
-        } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Potential KOL: " + kolName);
+            System.out.println("Tweet's posted date: " + postedDate);
         }
     }
 
-    private void navigateAndExtractUserData(String userLink) {
-        try {
-            driver.get(userLink);
-            Thread.sleep(5000);
-            WebElement userNameElement = wait.until(presenceOfElementLocated(
-                    By.xpath("//div[contains(@data-testid, 'UserName')]//span/span")));
-            String username = userNameElement.getText();
+    private void extractUserData(String userLink) throws InterruptedException {
+        WebElement userNameElement = wait.until(presenceOfElementLocated(
+                By.xpath("//div[contains(@data-testid, 'UserName')]//span/span")));
+        String username = userNameElement.getText();
 
-            WebElement followingCountElement = wait.until(presenceOfElementLocated(
-                    By.xpath("//a[contains(@href, 'following')]//span/span")));
-            int followingCount = toInt(followingCountElement.getText());
+        WebElement followingCountElement = wait.until(presenceOfElementLocated(
+                By.xpath("//a[contains(@href, 'following')]//span/span")));
+        int followingCount = toInt(followingCountElement.getText());
 
-            WebElement followersCountElement = wait.until(presenceOfElementLocated(
-                    By.xpath("//a[contains(@href, 'followers')]//span/span")));
-            int followersCount = toInt(followersCountElement.getText());
+        WebElement followersCountElement = wait.until(presenceOfElementLocated(
+                By.xpath("//a[contains(@href, 'followers')]//span/span")));
+        int followersCount = toInt(followersCountElement.getText());
 
-            System.out.println("User: " + username);
-            System.out.println("Following: " + followingCount);
-            System.out.println("Followers: " + followersCount);
+        System.out.println("User: " + username);
+        System.out.println("Following: " + followingCount);
+        System.out.println("Followers: " + followersCount);
 
-
-            extractPotentialKOLs();
-
-        } catch (Exception e) {
-            System.out.println("Unable to retrieve data for user at " + userLink);
-            e.printStackTrace();
-        }
+        extractPotentialKOLs();
     }
 
-    public void processAllUsersFromJson(String jsonFilePath) {
+    public void processAllUsersFromJson(String jsonFilePath) throws InterruptedException, IOException {
         List<String> userLinks = getUserLinksFromJson(jsonFilePath);
 
         System.out.println("Number of userLinks found: " + userLinks.size());
 
         for (String userLink : userLinks) {
             System.out.println("Processing user: " + userLink);
-            navigateAndExtractUserData(userLink);
-            try {
-                Thread.sleep(5000);  // Wait to avoid rate limits
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
+            driver.get(userLink);
+            Thread.sleep(5000);
+
+            extractUserData(userLink);
         }
     }
 
-    public void solve() {
+    public void scrape() throws InterruptedException, IOException {
         extractInitialKOLsToJson("potential_KOLs.json");
         processAllUsersFromJson("potential_KOLs.json");
     }
