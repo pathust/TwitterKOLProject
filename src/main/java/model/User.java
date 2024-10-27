8b62bf66c417dae766d5cc8e8543f315bd5d8680
Anh Tai Phan <taiphananh28@gmail.com>
2024-10-27 18:59:42 +0700

    remove followerList

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,11 +2,12 @@ package model;
 
 import java.util.List;
 
+import static java.lang.Double.parseDouble;
+
 public class User {
     private String username;
     private int followersCount;
-    private List<String> KOLfollowers;
-    private List<String> KOLfollowing;
+    private List<String> followingList;
 
     public User(String username, int followersCount) {
         this.username = username;
@@ -27,6 +28,22 @@ public class User {
 
     public void setFollowersCount(int followersCount) {
         this.followersCount = followersCount;
+    }
+
+    public void setFollowersCount(String followersCount) {
+        this.followersCount = toInt(followersCount);
+    }
+
+    public static int toInt(String followersCount) {
+        int factor = 1;
+        if (followersCount.endsWith("K")) {
+            followersCount = followersCount.replace("K", "");
+            factor = 1000;
+        } else if (followersCount.endsWith("M")) {
+            followersCount = followersCount.replace("M", "");
+            factor = 1000_000;
+        }
+        return (int)parseDouble(followersCount) * factor;
     }
 
     @Override
