f84af44d8eae0f5228694324573d2a2b3be7a5b2
Anh Tai Phan <taiphananh28@gmail.com>
2024-10-27 19:04:48 +0700

    add navigator

--- src/main/java/scraper/TwitterScraperController.java
+++ src/main/java/scraper/TwitterScraperController.java
@@ -6,19 +6,19 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TwitterScraperController {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "D:\\Test Java\\Selenium\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/Users/phananhtai/Downloads/chromedriver-mac-arm64/chromedriver");
         WebDriver driver = new ChromeDriver();
+        Navigator navigator = new Navigator(driver);
 
-        TwitterLogin login = new TwitterLogin(driver);
+        TwitterLogin login = new TwitterLogin(driver, navigator);
         login.login("@21Oop36301", "penaldomessy21@gmail.com", "123456789@21oop");
-        //login.login("PogbaPaul432283", "anhrooneymtp@gmail.com", "anhmanunited");
 
-        TwitterFilter filter = new TwitterFilter(driver);
+        TwitterFilter filter = new TwitterFilter(driver, navigator);
         filter.searchHashtagWithAdvancedFilters("#blockchain", 1000, 500, 200);
         System.out.println("Done");
 
-        TwitterScraper scraper = new TwitterScraper(driver);
-        scraper.processSearchResults();
+        TwitterScraper scraper = new TwitterScraper(driver, navigator);
+        scraper.solve();
         System.out.println("Done");
 //        driver.quit();
     }
