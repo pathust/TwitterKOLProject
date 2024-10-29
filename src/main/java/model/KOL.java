28125b953afb6f28a71c2ac24666cdaddd866fee
pathust <pathust28@gmail.com>
2024-10-29 14:45:20 +0700

    Revert "Dung"

--- src/main/java/model/KOL.java
+++ src/main/java/model/KOL.java
@@ -3,6 +3,8 @@ package model;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.util.List;
+
 public class KOL extends User{
     private Double pagerankScore;
     Map<KOL, Double> Interaction = new HashMap<>();
