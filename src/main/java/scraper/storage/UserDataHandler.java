f4978d74ab49b80a2ac5bbb0de7222ad4f529fef
Anh Tai Phan <taiphananh28@gmail.com>
2024-10-30 23:47:52 +0700

    add loadUsers

--- src/main/java/scraper/storage/UserDataHandler.java
+++ src/main/java/scraper/storage/UserDataHandler.java
@@ -7,7 +7,8 @@ import java.io.IOException;
 import java.util.List;
 
 public interface UserDataHandler {
+    void loadUsers(String filePath) throws IOException;
     void addUser(String filePath, User newUser) throws IOException;
     void saveData(String filePath) throws IOException;
-    List<String> getUserLinksFrom(String filePath) throws IOException;
+    List<User> getUsers(String filePath) throws IOException;
 }
