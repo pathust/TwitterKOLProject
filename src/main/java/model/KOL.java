bce3fa3d3d4536be376820f1e113084eb5620246
Anh Tai Phan <taiphananh28@gmail.com>
2024-11-05 21:01:51 +0700

    refactor: remove unused constructor, add attribute followingCount.

--- src/main/java/model/KOL.java
+++ src/main/java/model/KOL.java
@@ -10,12 +10,8 @@ public class KOL extends User{
     Map<KOL, Double> Interaction = new HashMap<>();
     private Double pagerankScore;
 
-    public KOL(String profileLink, int followersCount, List<User> followingList) {
-        super(profileLink, followersCount, followingList);
-    }
-
-    public KOL(String username, String profileLink, boolean isVerified){
-        super(username, profileLink, isVerified);
+    public KOL(String profileLink, String username, boolean isVerified){
+        super(profileLink, username, isVerified);
     }
 
     public Map<KOL, Double> getInteraction() {
