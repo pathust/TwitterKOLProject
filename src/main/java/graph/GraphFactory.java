79e15dca61c097760cfb6e7eb6c4348355616877
dungnguyen <dungnguyen>
2024-11-27 14:36:05 +0700

    no

--- src/main/java/graph/GraphFactory.java
+++ src/main/java/graph/GraphFactory.java
@@ -1,8 +1,6 @@
 package graph;
 
-import model.GraphNode;
-import model.Tweet;
-import model.User;
+import model.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,9 +15,11 @@ public class GraphFactory {
     public static final double repostWeight = 1.0;
 
     private final UserDataHandler userDataHandler;
+    private final NodeDataHandle nodeDataHandle;
 
     public GraphFactory() {
         this.userDataHandler = new UserStorageManager();
+        this.nodeDataHandle = new GraphNodeStorage();
     }
 
     public void loadUsers(String filePath) throws IOException {
@@ -30,6 +30,22 @@ public class GraphFactory {
         return userDataHandler.getUser(filePath, profileLink);
     }
 
+    public void addNode(User user, GraphNode userNode) throws IOException {
+        nodeDataHandle.addNode(user, userNode);
+    }
+
+    public void addNode(Tweet tweet, GraphNode tweetNode) throws IOException {
+        nodeDataHandle.addNode(tweet, tweetNode);
+    }
+
+    public GraphNode getNode(User user) throws IOException {
+        return nodeDataHandle.getNode(user);
+    }
+
+    public GraphNode getNode(Tweet tweet) throws IOException {
+        return nodeDataHandle.getNode(tweet);
+    }
+
     public static Graph createGraph(List<GraphNode> userNodeList, List<GraphNode> tweetNodeList) throws IOException {
         GraphFactory graphFactory = new GraphFactory();
         Graph graph = new Graph();
@@ -38,6 +54,9 @@ public class GraphFactory {
         // add user node
         for (GraphNode node : userNodeList) {
             graph.addNode(node);
+
+            User user = graphFactory.getUser("KOLs.json", node.getKol().getProfileLink());
+            graphFactory.addNode(user, node);
         }
 
         // add following edge
@@ -45,8 +64,9 @@ public class GraphFactory {
             User userNode = node.getKol();
             List<String> followingList = userNode.getFollowersList();
 
-            for(String userLink : followingList) {
-                GraphNode targetNode = new GraphNode(graphFactory.getUser("KOLs.json", userLink));
+            for(String targetUserLink : followingList) {
+                User targetUser = graphFactory.getUser("KOLs.json", targetUserLink);
+                GraphNode targetNode = graphFactory.getNode(targetUser);
                 graph.addEdge(node, targetNode, followWeight);
             }
         }
