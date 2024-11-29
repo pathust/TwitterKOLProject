72d9a4e7dcb83462a3cecf00a8e9611f1452a42d
linhmeme <linh.ht235135@sis.hust.edu.vn>
2024-11-29 23:08:24 +0700

    fxml

---
+++ src/main/java/UI/controller/home1.java
@@ -0,0 +1,26 @@
+package UI.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Button;
+
+
+import java.awt.*;
+
+public class home1 {
+    @FXML
+    private Button getStartedButton;
+
+    public void GetStarted() {
+
+    }
+
+    public Button getGetStartedButton() {
+        return getStartedButton;
+    }
+
+    public void setGetStartedButton(Button getStartedButton) {
+        this.getStartedButton = getStartedButton;
+    }
+}
--- src/main/java/scraper/TwitterScraperController.java
+++ src/main/java/scraper/TwitterScraperController.java
@@ -40,7 +40,7 @@ public class TwitterScraperController {
     public TwitterScraperController() {
         System.setProperty(
                 "webdriver.chrome.driver",
-                "/Users/phananhtai/Downloads/chromedriver-mac-arm64/chromedriver");
+                "C://Users//pc11w//Downloads//chromedriver-win64//chromedriver-win64//chromedriver.exe");
         driver = new ChromeDriver();
         this.navigator = new WebNavigator(driver);
         this.authenticator = new TwitterAuthenticator(driver, navigator);
@@ -138,7 +138,7 @@ public class TwitterScraperController {
     public static void main(String[] args) throws IOException, InterruptedException {
         TwitterScraperController controller = new TwitterScraperController();
 
-        controller.login();
+        controller.login("hehele47504","linh.ht235135@sis.hust.edu.vn ","Linh166105@");
         controller.applyFilter(
                 List.of(args),
                 1000,
