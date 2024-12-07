ce9da11ca6cab06c78a033c969e88b91d2ac2be2
Sonspeed <son.dt0430@gmail.com>
2024-12-07 15:40:22 +0700

    refactor KOLTable

--- src/main/java/UI/display/KOLTable.java
+++ src/main/java/UI/display/KOLTable.java
@@ -1,21 +1,31 @@
 package UI.display;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
 import model.User;
 
+import java.util.function.Function;
+
 public class KOLTable {
+
+    // Hàm tạo cột chung
+    public <T> TableColumn<User, T> createColumn(String title, Function<User, T> mapper) {
+        TableColumn<User, T> column = new TableColumn<>(title);
+        column.setCellValueFactory(cellData ->
+                new SimpleObjectProperty<>(mapper.apply(cellData.getValue()))
+        );
+        return column;
+    }
 
     public TableView<User> createTable(ObservableList<User> data) {
         TableView<User> tableView = new TableView<>();
 
-        // Cột Username
-        TableColumn<User, String> usernameCol = new TableColumn<>("Username");
-        usernameCol.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getUsername()));
+        // Tạo các cột bằng hàm chung
+        TableColumn<User, String> usernameCol = createColumn("Username", User::getUsername);
 
-        // Cột Profile Link
-        TableColumn<User, String> profileLinkCol = new TableColumn<>("Profile Link");
-        profileLinkCol.setCellValueFactory(cellData -> new javafx.beans.property.SimpleStringProperty(cellData.getValue().getProfileLink()));
+        TableColumn<User, String> profileLinkCol = createColumn("Profile Link", User::getProfileLink);
         profileLinkCol.setCellFactory(col -> new TableCell<>() {
             @Override
             protected void updateItem(String item, boolean empty) {
@@ -36,13 +46,8 @@ public class KOLTable {
             }
         });
 
-        // Cột Following Count
-        TableColumn<User, Integer> followingCountCol = new TableColumn<>("Following Count");
-        followingCountCol.setCellValueFactory(cellData -> new javafx.beans.property.SimpleObjectProperty<>(cellData.getValue().getFollowingCount()));
-
-        // Cột Followers Count
-        TableColumn<User, Integer> followersCountCol = new TableColumn<>("Followers Count");
-        followersCountCol.setCellValueFactory(cellData -> new javafx.beans.property.SimpleObjectProperty<>(cellData.getValue().getFollowersCount()));
+        TableColumn<User, Integer> followingCountCol = createColumn("Following Count", User::getFollowingCount);
+        TableColumn<User, Integer> followersCountCol = createColumn("Followers Count", User::getFollowersCount);
 
         // Thêm các cột vào bảng
         tableView.getColumns().addAll(usernameCol, profileLinkCol, followingCountCol, followersCountCol);
