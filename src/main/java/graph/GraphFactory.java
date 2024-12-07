a8a935631d9e2e3901864e4e9a203a5c79c1c63e
dungnguyen <dungnguyen>
2024-12-07 11:46:34 +0700

    update graph

--- src/main/java/graph/GraphFactory.java
+++ src/main/java/graph/GraphFactory.java
@@ -5,32 +5,20 @@ import model.*;
 import java.io.IOException;
 import java.util.List;
 
-import storage.main.StorageHandler;
-import utils.ObjectType;
 
 public class GraphFactory {
     public static final double followWeight = 1.0;
     public static final double postWeight = 1.0;
     public static final double repostWeight = 1.0;
 
-    private final StorageHandler storageHandler;
     private final GraphNodeStorage graphNodeStorage;
 
     public GraphFactory() {
-        this.storageHandler = new StorageHandler();
         this.graphNodeStorage = new GraphNodeStorage();
     }
 
-    public void load(ObjectType type, String filePath) throws IOException {
-        storageHandler.load(type, filePath);
-    }
-
-    public DataModel get(ObjectType type, String filePath, String uniqueKey) throws IOException {
-        return storageHandler.get(type, filePath, uniqueKey);
-    }
-
-    public void addNode(String uniqueKey, GraphNode userNode) {
-        graphNodeStorage.addNode(uniqueKey, userNode);
+    public void addNode(String uniqueKey, GraphNode node) {
+        graphNodeStorage.addNode(uniqueKey, node);
     }
 
     public GraphNode getNode(String uniqueKey) {
@@ -40,47 +28,42 @@ public class GraphFactory {
     public static Graph createGraph(List<GraphNode> userNodeList, List<GraphNode> tweetNodeList) throws IOException {
         GraphFactory graphFactory = new GraphFactory();
         Graph graph = new Graph();
-        graphFactory.load(ObjectType.USER, "KOLs.json");
 
         // add user node
         for (GraphNode node : userNodeList) {
             graph.addNode(node);
 
-            User user = (User) graphFactory.get(ObjectType.USER, "KOLs.json", node.getKol().getProfileLink());
-            graphFactory.addNode(user, node);
+            String userLink = node.getDataModel().getUniqueKey();
+            graphFactory.addNode(userLink, node);
         }
 
         // add following edge
         for (GraphNode node : userNodeList) {
-            User userNode = node.getKol();
+            User userNode = (User) node.getDataModel();
             List<String> followersList = userNode.getFollowersList();
 
             for(String followerLink : followersList) {
-                User followerUser = (User) graphFactory.get(ObjectType.USER, "KOLs.json", followerLink);
-                GraphNode followerNode = graphFactory.getNode(followerUser);
+                GraphNode followerNode = graphFactory.getNode(followerLink);
                 graph.addEdge(followerNode, node, followWeight);
             }
         }
 
-        // ADD NEEDED USER NODE
-
         // add tweet node
         for (GraphNode tweetNode : tweetNodeList) {
-            Tweet tweet = tweetNode.getTweet();
-            User userPost = (User) graphFactory.get(ObjectType.USER, "KOLs.json", tweet.getAuthorProfileLink());  //  lỗi chưa co link trong KOL.json
-            GraphNode userPostNode = graphFactory.getNode(userPost);
+            Tweet tweet = (Tweet) tweetNode.getDataModel();
+            String authorProfileLink = tweet.getAuthorProfileLink();  //  lỗi chưa co link trong KOL.json
+            GraphNode authorNode = graphFactory.getNode(authorProfileLink);
 
             // add tweet node, user post node
             graph.addNode(tweetNode);
-            graph.addNode(userPostNode);
+            graph.addNode(authorNode);
 
             // add post edge
-            graph.addEdge(tweetNode, userPostNode, postWeight);
+            graph.addEdge(tweetNode, authorNode, postWeight);
 
             // add repost edge
             for(String userRepostLink : tweet.getRepostList()) {
-                User userRepost = (User) graphFactory.get(ObjectType.USER, "KOLs.json", userRepostLink);  //  lỗi chưa co link trong KOL.json
-                GraphNode userRepostNode = graphFactory.getNode(userRepost);
+                GraphNode userRepostNode = graphFactory.getNode(userRepostLink);
 
                 graph.addNode(userRepostNode);
                 graph.addEdge(userRepostNode, tweetNode, repostWeight);
